55c5e8d169a8315041b7d1b27ef30532bd133dc4
raimoa1 <raimo.ainla@gmail.com>
2019-04-27 18:26:37 +0300

    adding class, constructor and initializing state

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-const App = () => {
+//class-based component for use of state
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {lat: null};
+  }
+
+  // React says we have to define render (otherwise React throws an error)
+  render() {
+    //callback function to get user's location. If fails, log error
     window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
+      position => console.log(position),
+      err => console.log(err)
     );
 
-
-    return (
-      <div>Hi there!</div>
-    );
-};
-
+    return <div>Latitude:</div>
+  }
+}
 
 ReactDOM.render(
-    <App/>,
-    document.querySelector('#root')
+  <App/>,
+  document.querySelector('#root')
 );
